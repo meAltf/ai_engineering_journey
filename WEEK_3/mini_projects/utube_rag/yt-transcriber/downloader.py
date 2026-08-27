63ed4650aeb5426b4aca7935b43c3be3564fb411
meAltf <alataf.dce.cs1@gmail.com>
2026-08-27 16:04:21 +0530

    Implemented downloader logic

---
+++ WEEK_3/mini_projects/utube_rag/yt-transcriber/downloader.py
@@ -0,0 +1,24 @@
+import subprocess
+import os
+
+def download_audio(url, audio_dir):
+    os.makedirs(audio_dir, exist_ok=True)
+
+    output_template = os.path.join(audio_dir, "%(title)s.%(ext)s")
+
+    subprocess.run([
+        "yt-dlp",
+        "-f", "bestaudio",
+        "--extract-audio",
+        "--audio-format", "mp3",
+        "-o", output_template,
+        url
+    ], check=True)
+
+    # return latest file (simple approach)
+    files = os.listdir(audio_dir)
+    files = [f for f in files if f.endswith(".mp3")]
+    
+    latest_file = max(files, key=lambda x: os.path.getctime(os.path.join(audio_dir, x)))
+
+    return os.path.join(audio_dir, latest_file)
