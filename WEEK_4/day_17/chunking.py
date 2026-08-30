b3b1076938059b84a9e0a1ebfdee8e7bde2923dc
meAltf <alataf.dce.cs1@gmail.com>
2026-08-30 23:53:53 +0530

    Implemented | recursive splitting chunking

--- WEEK_4/day_17/chunking.py
+++ WEEK_4/day_17/chunking.py
@@ -1,4 +1,5 @@
 from texts import sample_text
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 # 1. Fixed size chunking
 def fixed_size_chunk(text, chunk_size=100, overlap=20):
@@ -19,7 +20,24 @@ def fixed_size_chunk(text, chunk_size=100, overlap=20):
 
 chunks = fixed_size_chunk(sample_text, chunk_size=100, overlap=20)
 
+print(f"-------------- Fixed size chunking --------------------\n")
 for i, chunk in enumerate(chunks):
     print(f"--- Chunk {i+1} ---")
     print(chunk)
+    print()
+
+
+# 2. Recursive splitting | using LangChain
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=150,      
+    chunk_overlap=20,    # characters shared between chunks
+    separators=["\n\n", "\n", ". ", " "]   # order of preference for splitting
+)
+
+chunks = splitter.split_text(sample_text)
+
+print(f"-------------- Recursive chunking --------------------\n")
+for i, c in enumerate(chunks):
+    print(f"--- Chunk {i+1} ---")
+    print(c)
     print()
