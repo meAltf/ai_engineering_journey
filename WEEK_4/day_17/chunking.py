258a81f2589bbabcd1e7f3eac6d0ac5ff2fe9a6f
meAltf <alataf.dce.cs1@gmail.com>
2026-08-31 00:00:21 +0530

    implemented | sentence based chunking

--- WEEK_4/day_17/chunking.py
+++ WEEK_4/day_17/chunking.py
@@ -1,5 +1,7 @@
 from texts import sample_text
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+import nltk
+from nltk.tokenize import sent_tokenize
 
 # 1. Fixed size chunking
 def fixed_size_chunk(text, chunk_size=100, overlap=20):
@@ -41,3 +43,30 @@ for i, c in enumerate(chunks):
     print(f"--- Chunk {i+1} ---")
     print(c)
     print()
+
+
+
+# 3. Sentence-based chunking  | grouping N sentences together
+# this downloads the sentence-splitting model (only needed once)
+nltk.download('punkt')
+
+def sentence_chunk(text, sentences_per_chunk=2):
+    sentences = sent_tokenize(text)   # split text into a list of sentences
+    chunks = []
+
+    # grouping them N at a time
+    for i in range(0, len(sentences), sentences_per_chunk):
+        group = sentences[i:i + sentences_per_chunk]   # take N sentences
+        chunk = " ".join(group)                         
+        chunks.append(chunk)
+
+    return chunks
+
+chunks = sentence_chunk(sample_text, sentences_per_chunk=2)
+
+print(f"-------------- sentence based chunking --------------------\n")
+for i, chunk in enumerate(chunks):
+    print(f"--- Chunk {i+1} ---")
+    print(chunk)
+    print()
+
