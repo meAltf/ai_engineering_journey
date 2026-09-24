0b9cb85baefb0752511f9f5393f13929f406aa23
meAltf <alataf.dce.cs1@gmail.com>
2026-09-24 22:16:13 +0530

    added web search capabilites to agent

--- WEEK_5/day_22/multi_agent.py
+++ WEEK_5/day_22/multi_agent.py
@@ -3,6 +3,7 @@ import os
 
 from dotenv import load_dotenv
 from groq import Groq
+from tavily import TavilyClient
 
 load_dotenv()
 model_openai = "openai/gpt-oss-120b"
@@ -14,6 +15,10 @@ groq_client = Groq(api_key = GROQ_API_KEY)
 if not GROQ_API_KEY:
     raise ValueError(f'There is no api key with name GROQ_API_KEY in env file')
 
+tavily_client = TavilyClient(api_key = TAVILY_API_KEY)
+if not TAVILY_API_KEY:
+    raise ValueError(f'There is no api key with name TAVILY_API_KEY in en file')
+
 
 def llm_response(prompt):
     response = groq_client.chat.completions.create(
@@ -23,6 +28,19 @@ def llm_response(prompt):
     )
     return response.choices[0].message.content
 
+# web search
+def tavily_search(user_query):
+    tavily_response = tavily_client.search(query = user_query, max_result = 2)
+    # print(f' tavily responses are: {tavily_response}')
+
+    tavily_results = []
+    for item in tavily_response.get("results", []):
+        content = item.get("content", "")
+        tavily_results.append(content)
+
+    final_response = "\n".join(tavily_results)
+    return final_response
+
 
 # research agent:
 def research_agent(query, context):
@@ -30,6 +48,8 @@ def research_agent(query, context):
     You are a genius research agent.
     User question: {query}
     Current context: {context}
+
+    if you don't find any answer please use this function to web search: {tavily_search(query)}
 
     Do you have enough information?
     Reply only in JSON:
